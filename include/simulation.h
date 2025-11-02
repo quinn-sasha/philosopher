2171dd0358aae98669d76af50069be9ca996f77b
quinn-sasha <quinnsashakenta@gmail.com>
2025-11-02 12:06:45 +0900

    Fixed wrong funtion prototype

--- include/simulation.h
+++ include/simulation.h
@@ -1,9 +1,14 @@
 #ifndef SIMULATION_H
 #define SIMULATION_H
 
+// moniotr.c
 #define MONITOR_INTERVAL_IN_USEC 1000
 
-bool unsafe_is_hungry(t_philo philo, int max_eat);
+// unsafe_functions.c
+bool unsafe_is_hungry(t_philo philo);
 bool unsafe_is_dead(t_philo philo, t_timeval *checked_at);
+
+// log.c
+void write_dead_log(t_philo *philo, t_timeval died_at);
 
 #endif
