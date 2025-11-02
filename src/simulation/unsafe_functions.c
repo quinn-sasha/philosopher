a2401c1f3ca1dd414aa2707e5973b6e465c6fdad
quinn-sasha <quinnsashakenta@gmail.com>
2025-11-02 10:33:32 +0900

    Added unsafe_is_hungry() and unsafe_is_dead()

---
+++ src/simulation/unsafe_functions.c
@@ -0,0 +1,21 @@
+#include "philo.h"
+#include "utils.h"
+
+bool unsafe_is_hungry(t_philo philo) {
+  const int max_eat = philo.data->args.max_eat;
+  if (max_eat < 0)
+    return true;
+  return philo.eat_count < max_eat;
+}
+
+bool unsafe_is_dead(t_philo philo, t_timeval *checked_at) {
+  t_timeval now;
+  gettimeofday(&now, NULL);
+  rounddown_msec(&now);
+  const int time_to_die_ms = philo.data->args.time_to_die_ms;
+  t_timeval deadline = timeadd_msec(philo.last_eat_at, time_to_die_ms);
+  suseconds_t diff = timediff_usec(now, deadline);
+  if (diff < 0)
+    return true;
+  return false;
+}
