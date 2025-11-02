e81bf4e7375b8d73266210ea88c088868ad57e78
quinn-sasha <quinnsashakenta@gmail.com>
2025-11-02 12:10:56 +0900

    Added comment

--- src/simulation/unsafe_functions.c
+++ src/simulation/unsafe_functions.c
@@ -8,6 +8,8 @@ bool unsafe_is_hungry(t_philo philo) {
   return philo.eat_count < max_eat;
 }
 
+// nowのmsec以下を切り捨てている理由は、deadlineの方ではmsecより下が切り捨てられているから
+// より比べるときの公平性が高い
 bool unsafe_is_dead(t_philo philo, t_timeval *checked_at) {
   t_timeval now;
   gettimeofday(&now, NULL);
