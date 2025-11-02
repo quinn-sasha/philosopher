82b54f17aac9e3d91d09081fc331f97ca9a8d6c3
quinn-sasha <quinnsashakenta@gmail.com>
2025-11-02 19:14:16 +0900

    Added: create_philosophers_thread()

--- src/simulation/simulation.c
+++ src/simulation/simulation.c
@@ -6,12 +6,17 @@ static void create_monitor_thread(t_data *data) {
   pthread_create(&data->monitor.tid, NULL, monitor_thread, data);
 }
 
-static void create_philosophers_thread() {
-
+static void create_philosophers_thread(t_philo *philo) {
+  const int num_philo = philo->data->args.num_philo;
+  int i = 0;
+  while (i < num_philo) {
+    pthread_create(&philo[i].tid, NULL, philosopher_thread, &philo[i]);
+    i++;
+  }
 }
 
 void start_simulation(t_data *data) {
   create_monitor_thread(data);
-  create_philosophers_thread();
+  create_philosophers_thread(data->philosophers);
 }
 
