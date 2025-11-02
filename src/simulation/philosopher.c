fdd37eb7725e1fd87efc5ab7072f25df1deba9ad
quinn-sasha <quinnsashakenta@gmail.com>
2025-11-02 19:00:02 +0900

    Added: philo_sleep()

--- src/simulation/philosopher.c
+++ src/simulation/philosopher.c
@@ -25,6 +25,7 @@ static bool pickup_forks(t_philo *philo) {
 }
 
 static bool philo_eat(t_philo *philo) {
+  const int time_to_eat_ms = philo->data->args.time_to_eat_ms;
   philo->state = PH_EATING;
   bool is_end = pickup_forks(philo);
   if (is_end)
@@ -36,9 +37,18 @@ static bool philo_eat(t_philo *philo) {
   philo->next_eat_at = timeadd_msec(philo->last_eat_at,
                                     philo->data->optimal_interval_ms);
   if (!is_end)
-    usleep_since(philo->last_eat_at, philo->data->args.time_to_eat_ms);
+    usleep_since(philo->last_eat_at, time_to_eat_ms);
   pthread_mutex_unlock(philo->second);
   pthread_mutex_unlock(philo->first);
+  return is_end;
+}
+
+static bool philo_sleep(t_philo *philo) {
+  const int time_to_sleep_ms = philo->data->args.time_to_sleep_ms;
+  philo->state = PH_SLEEPING;
+  bool is_end = unsafe_write_log(philo, "is sleeping", &philo->last_sleep_at);
+  if (!is_end)
+    usleep_since(philo->last_sleep_at, time_to_sleep_ms);
   return is_end;
 }
 
@@ -52,9 +62,9 @@ void *philosopher_thread(void *arg) {
   bool is_end = false;
   while (!is_end && unsafe_is_hungry(*philo)) {
     if (philo->state == PH_THINKING)
-      is_end = philo_eat();
+      is_end = philo_eat(philo);
     else if (philo->state == PH_EATING)
-      is_end = philo_sleep();
+      is_end = philo_sleep(philo);
     else if (philo->state == PH_SLEEPING)
       is_end = philo_think();
     else
