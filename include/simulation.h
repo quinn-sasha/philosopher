0a6241fc474db00723c5d1cb9f353812e462a5ee
quinn-sasha <quinnsashakenta@gmail.com>
2025-11-07 09:10:21 +0900

    Added function prototypes in header file

--- include/simulation.h
+++ include/simulation.h
@@ -5,6 +5,9 @@
 #define MONITOR_INTERVAL_IN_USEC 1000
 void *monitor_thread(void *arg);
 
+// philosopher.c
+void *philosopher_thread(void *arg);
+
 // unsafe_functions.c
 bool unsafe_is_hungry(t_philo philo);
 bool unsafe_is_dead(t_philo philo, t_timeval *checked_at);
@@ -12,5 +15,8 @@ bool unsafe_is_dead(t_philo philo, t_timeval *checked_at);
 // log.c
 void write_dead_log(t_philo *philo, t_timeval died_at);
 bool unsafe_write_log(t_philo *philo, const char *message, t_timeval *tp);
+
+// simulation.c
+void start_simulation(t_data *data);
 
 #endif
