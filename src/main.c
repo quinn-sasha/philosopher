8abb23b3355ae04dffb4eecd4b072cbc4b7522d0
quinn-sasha <quinnsashakenta@gmail.com>
2025-11-07 09:49:43 +0900

    Implemented mian function

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include "philo.h"
 #include "parse_argument.h"
+#include "simulation.h"
 
 int main(int argc, char *argv[]) {
   t_data data;
   if (parse_argsument(&data.args, argc, argv) == PARSE_ERROR)
     return EXIT_FAILURE;
   init_data(&data);
-  // start simulation
-  // wait simulation ends
+  start_simulation(&data);
+  wait_simulation_ends(data);
   return EXIT_SUCCESS;
 }
