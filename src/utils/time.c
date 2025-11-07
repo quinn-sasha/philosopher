6b848356f0b3d8b3f2e6acf22648e08e49fb0118
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-11-07 16:13:58 +0900

    Modified timeadd_msec(): 変数を増やして、より説明的にした

--- src/utils/time.c
+++ src/utils/time.c
@@ -6,11 +6,12 @@ t.tv_usec += offset_ms * MSEC_IN_USEC;
 というコードは offset_ms が INT_MAX に近い時にオーバーフローが起こる可能性がある
 */
 t_timeval timeadd_msec(t_timeval t, int offset_ms) {
-  const int offset_only_ms = offset_ms % SEC_IN_MSEC;
-  t.tv_usec += offset_only_ms * MSEC_IN_USEC;
-  t.tv_sec += offset_ms / SEC_IN_MSEC;
+  const int remainder_msec = offset_ms % SEC_IN_MSEC;
+  const int quotient_sec  = offset_ms / SEC_IN_MSEC;
+  t.tv_usec += remainder_msec * MSEC_IN_USEC;
   t.tv_sec += t.tv_usec / SEC_IN_USEC;
   t.tv_usec %= SEC_IN_USEC;
+  t.tv_sec += quotient_sec;
   return t;
 }
 
