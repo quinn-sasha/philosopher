d3f36d4fe33111ae3e05cc38abde9148918a5330
quinn-sasha <quinnsashakenta@gmail.com>
2025-11-08 18:43:23 +0900

    Fixed norm

--- src/utils/sleep.c
+++ src/utils/sleep.c
@@ -1,18 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sleep.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/08 18:38:16 by squinn            #+#    #+#             */
+/*   Updated: 2025/11/08 18:38:17 by squinn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 #include "utils.h"
 
-void usleep_until(t_timeval end) {
-  t_timeval now;
-  while (true) {
-    gettimeofday(&now, NULL);
-    suseconds_t diff = timediff_usec(now, end);
-    if (diff <= 0)
-      return;
-    usleep(diff / 2);
-  }
+void	usleep_until(t_timeval end)
+{
+	t_timeval	now;
+	suseconds_t	diff;
+
+	while (true)
+	{
+		gettimeofday(&now, NULL);
+		diff = timediff_usec(now, end);
+		if (diff <= 0)
+			return ;
+		usleep(diff / 2);
+	}
 }
 
-void usleep_since(t_timeval start, int length_ms) {
-  t_timeval end = timeadd_msec(start, length_ms);
-  usleep_until(end);
+void	usleep_since(t_timeval start, int length_ms)
+{
+	t_timeval	end;
+
+	end = timeadd_msec(start, length_ms);
+	usleep_until(end);
 }
--- src/utils/time.c
+++ src/utils/time.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/08 18:38:22 by squinn            #+#    #+#             */
+/*   Updated: 2025/11/08 18:42:22 by squinn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 #include "utils.h"
 
@@ -5,31 +17,40 @@
 t.tv_usec += offset_ms * MSEC_IN_USEC;
 というコードは offset_ms が INT_MAX に近い時にオーバーフローが起こる可能性がある
 */
-t_timeval timeadd_msec(t_timeval t, int offset_ms) {
-  const int remainder_msec = offset_ms % SEC_IN_MSEC;
-  const int quotient_sec  = offset_ms / SEC_IN_MSEC;
-  t.tv_usec += remainder_msec * MSEC_IN_USEC;
-  t.tv_sec += t.tv_usec / SEC_IN_USEC;
-  t.tv_usec %= SEC_IN_USEC;
-  t.tv_sec += quotient_sec;
-  return t;
+t_timeval	timeadd_msec(t_timeval t, int offset_ms)
+{
+	const int	remainder_msec = offset_ms % SEC_IN_MSEC;
+	const int	quotient_sec = offset_ms / SEC_IN_MSEC;
+
+	t.tv_usec += remainder_msec * MSEC_IN_USEC;
+	t.tv_sec += t.tv_usec / SEC_IN_USEC;
+	t.tv_usec %= SEC_IN_USEC;
+	t.tv_sec += quotient_sec;
+	return (t);
 }
 
-int timediff_msec(t_timeval start, t_timeval end) {
-  int diff = 0;
-  diff += (int)(end.tv_sec - start.tv_sec) * SEC_IN_MSEC;
-  diff += (end.tv_usec - start.tv_usec) / MSEC_IN_USEC;
-  return diff;
+int	timediff_msec(t_timeval start, t_timeval end)
+{
+	int	diff;
+
+	diff = (int)(end.tv_sec - start.tv_sec);
+	diff *= SEC_IN_MSEC;
+	diff += (end.tv_usec - start.tv_usec) / MSEC_IN_USEC;
+	return (diff);
 }
 
-suseconds_t timediff_usec(t_timeval start, t_timeval end) {
-  suseconds_t diff = 0;
-  diff += (suseconds_t)(end.tv_sec - start.tv_sec) * SEC_IN_USEC;
-  diff += end.tv_usec - start.tv_usec;
-  return diff;
+suseconds_t	timediff_usec(t_timeval start, t_timeval end)
+{
+	suseconds_t	diff;
+
+	diff = 0;
+	diff += (suseconds_t)(end.tv_sec - start.tv_sec) * SEC_IN_USEC;
+	diff += end.tv_usec - start.tv_usec;
+	return (diff);
 }
 
-void rounddown_msec(t_timeval *tp) {
-  tp->tv_usec /= MSEC_IN_USEC;
-  tp->tv_usec *= MSEC_IN_USEC;
+void	rounddown_msec(t_timeval *tp)
+{
+	tp->tv_usec /= MSEC_IN_USEC;
+	tp->tv_usec *= MSEC_IN_USEC;
 }
