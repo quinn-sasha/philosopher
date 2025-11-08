e0e4689bcf3e28856692194fdc6e1a1d8cee1f81
quinn-sasha <quinnsashakenta@gmail.com>
2025-11-08 13:35:47 +0900

    Fixed: unsafe_write_dead_log() should be declared before unsafe_write_log()

--- src/simulation/log.c
+++ src/simulation/log.c
@@ -2,6 +2,24 @@
 #include "simulation.h"
 #include "utils.h"
 
+// It needs mutex for printed_dead_log and monitor.is_dead to write them
+// 死亡ログは一度しか表示されない
+static void unsafe_write_dead_log(t_philo *philo, t_timeval died_at) {
+  static bool printed_dead_log = false;
+  if (printed_dead_log)
+    return;
+  philo->data->monitor.is_dead = true;
+  int timestamp_ms = timediff_msec(philo->data->started_at, died_at);
+  printf("%d %d died\n", timestamp_ms ,philo->id);
+  printed_dead_log = true;
+}
+
+void write_dead_log(t_philo *philo, t_timeval died_at) {
+  pthread_mutex_lock(&philo->data->monitor.mutex);
+  unsafe_write_dead_log(philo, died_at);
+  pthread_mutex_unlock(&philo->data->monitor.mutex);
+}
+
 /*
 * It is unsafe if monitor tries to read last_eat_at without philo's mutex.
 * Return true if someone dies, otherwise false.
@@ -9,7 +27,7 @@
 bool unsafe_write_log(t_philo *philo, const char *message, t_timeval *tp) {
   bool is_end;
   t_timeval now;
-  ptrhead_mutex_lock(&philo->data->monitor.mutex);
+  pthread_mutex_lock(&philo->data->monitor.mutex);
   if (philo->data->monitor.is_dead)
     is_end = true;
   else
@@ -27,22 +45,4 @@ bool unsafe_write_log(t_philo *philo, const char *message, t_timeval *tp) {
   }
   pthread_mutex_unlock(&philo->data->monitor.mutex);
   return is_end;
-}
-
-// It needs mutex for printed_dead_log and monitor.is_dead to write them
-// 死亡ログは一度しか表示されない
-static void unsafe_write_dead_log(t_philo *philo, t_timeval died_at) {
-  static bool printed_dead_log = false;
-  if (printed_dead_log)
-    return;
-  philo->data->monitor.is_dead = true;
-  int timestamp_ms = timediff_msec(philo->data->started_at, died_at);
-  printf("%d %d died\n", timestamp_ms ,philo->id);
-  printed_dead_log = true;
-}
-
-void write_dead_log(t_philo *philo, t_timeval died_at) {
-  pthread_mutex_lock(&philo->data->monitor.mutex);
-  unsafe_write_dead_log(philo, died_at);
-  pthread_mutex_unlock(&philo->data->monitor.mutex);
 }
